57358b6c66bc7954d0d10b90b8503231374fbb67
superdyoll <superdyoll@gmail.com>
2017-11-22 23:33:24 +0000

    Logic written but null pointer exception

--- src/MaximillionGalactica.java
+++ src/MaximillionGalactica.java
@@ -7,182 +7,259 @@ import negotiator.actions.Offer;
 import negotiator.issue.*;
 import negotiator.parties.AbstractNegotiationParty;
 import negotiator.parties.NegotiationInfo;
+import negotiator.utility.AdditiveUtilitySpace;
+import negotiator.utility.EvaluatorDiscrete;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MaximillionGalactica extends AbstractNegotiationParty {
-	private String description = "Maximillion Galactica";
-	private double stubbornness = 10_000;
-	private int round = 0;
-	private Bid maxbid;
+    private String description = "Maximillion Galactica";
+    private double stubbornness = 10_000;
+    private int round = 0;
+    private Bid maxbid;
+    private Map<Bid, Double> discretePref;
 
-	private List<Pair<AgentID, Offer>> history = new ArrayList<>();
-	private Map<AgentID, Offer> agents = new HashMap<>();
+    private List<Pair<AgentID, Offer>> history = new ArrayList<>();
+    private Map<AgentID, Offer> agents = new HashMap<>();
 
-	/**
-	 * Lerp between two values a and b using t
-	 */
-	private int lerp(int a, int b, float t) {
-		return (int) (a + t * (b - a));
-	}
-	private int lerp(int a, int b, double t) {
-		return (int) (a + t * (b - a));
-	}
-	private float lerp(float a, float b, float t) {
-		return a + t * (b - a);
-	}
-	private double lerp(double a, double b, double t) {
-		return a + t * (b - a);
-	}
+    /**
+     * Lerp between two values a and b using t
+     */
+    private int lerp(int a, int b, float t) {
+        return (int) (a + t * (b - a));
+    }
 
-	/**
-	 * Clamp value x between min and max
-	 */
-	private int clamp(int x, int min, int max) {
-		return x < max ? (x > min ? x : min) : max;
-	}
-	private float clamp(float x, float min, float max) {
-		return x < max ? (x > min ? x : min) : max;
-	}
-	private double clamp(double x, double min, double max) {
-		return x < max ? (x > min ? x : min) : max;
-	}
-	private float clamp01(int x) {
-		return clamp(x, 0, 1);
-	}
-	private float clamp01(float x) {
-		return clamp(x, 0, 1);
-	}
-	private double clamp01(double x) {
-		return clamp(x, 0, 1);
-	}
+    private int lerp(int a, int b, double t) {
+        return (int) (a + t * (b - a));
+    }
 
-	/**
-	 * How much are we willing to conceed at time t?
-	 */
-	private double conceed(double t) {
-		return clamp01(- (Math.pow(stubbornness, clamp01(t)) / stubbornness) + 1);
-	}
+    private float lerp(float a, float b, float t) {
+        return a + t * (b - a);
+    }
 
-	@Override
-	public void init(NegotiationInfo info) {
-		super.init(info);
-	}
+    private double lerp(double a, double b, double t) {
+        return a + t * (b - a);
+    }
 
-	/**
-	 * When this function is called, it is expected that the Party chooses one of the actions from the possible
-	 * action list and returns an instance of the chosen action.
-	 */
-	@Override
-	public Action chooseAction(List<Class<? extends Action>> list) {
-		System.out.println(getDescription() + ": ChooseAction(" + list + ")");
+    /**
+     * Clamp value x between min and max
+     */
+    private int clamp(int x, int min, int max) {
+        return x < max ? (x > min ? x : min) : max;
+    }
 
-		if(maxbid == null) maxbid = this.getMaxUtilityBid(); // TODO REALLY FUCKING SLOW
+    private float clamp(float x, float min, float max) {
+        return x < max ? (x > min ? x : min) : max;
+    }
 
-		// According to Stacked Alternating Offers Protocol list includes
-		// Accept, Offer and EndNegotiation actions only.
-		double time = getTimeLine().getTime(); // Gets the time, running from t = 0 (start) to t = 1 (deadline).
-		double willingness = conceed(time);
-		// The time is normalized, so agents need not be
-		// concerned with the actual internal clock.
+    private double clamp(double x, double min, double max) {
+        return x < max ? (x > min ? x : min) : max;
+    }
 
-		// Increase our round timer.
-		++round;
+    private float clamp01(int x) {
+        return clamp(x, 0, 1);
+    }
 
-		// We need some time to understand our opponents
-		// Means we have enough data and we don't (really) need to worry about nulls
-		if(round <= 2) return new Offer(this.getPartyId(), maxbid);
+    private float clamp01(float x) {
+        return clamp(x, 0, 1);
+    }
 
-		// Last bid
-		Bid last = history.get(history.size()-1).getSecond().getBid();
+    private double clamp01(double x) {
+        return clamp(x, 0, 1);
+    }
 
-		// Find the average
-		Map<Integer, Value> averages = new HashMap<>(maxbid.getValues());
-		Map<Integer, Value> proposal = new HashMap<>(maxbid.getValues());
+    /**
+     * How much are we willing to conceed at time t?
+     */
+    private double conceed(double t) {
+        return clamp01(-(Math.pow(stubbornness, clamp01(t)) / stubbornness) + 1);
+    }
 
-		for(Map.Entry<Integer, Value> entry : averages.entrySet()) {
-			int id = entry.getKey();
-			Value value = entry.getValue();
+    @Override
+    public void init(NegotiationInfo info) {
+        super.init(info);
+    }
 
-			if (value instanceof ValueDiscrete) {
-				// TODO implement
-				System.err.println("ValueDiscrete is not implemented! Defaulting to max bid.");
-			} else if (value instanceof ValueInteger) {
-				int sum = 0;
-				int count = 0;
+    /**
+     * When this function is called, it is expected that the Party chooses one of the actions from the possible
+     * action list and returns an instance of the chosen action.
+     */
+    @Override
+    public Action chooseAction(List<Class<? extends Action>> list) {
+        System.out.println(getDescription() + ": ChooseAction(" + list + ")");
 
-				for (Map.Entry<AgentID, Offer> agent : agents.entrySet()) {
-					sum += ((ValueInteger) agent.getValue().getBid().getValue(id)).getValue();
-					++count;
-				}
+        if (maxbid == null) maxbid = this.getMaxUtilityBid(); // TODO: REALLY FUCKING SLOW
 
-				proposal.put(id, new ValueInteger(lerp(sum / count, ((ValueInteger) averages.get(id)).getValue(), willingness)));
-				averages.put(id, new ValueInteger(sum / count));
-			} else if (value instanceof ValueReal) {
-				double sum = 0;
-				int count = 0;
+        // According to Stacked Alternating Offers Protocol list includes
+        // Accept, Offer and EndNegotiation actions only.
+        double time = getTimeLine().getTime(); // Gets the time, running from t = 0 (start) to t = 1 (deadline).
+        double willingness = conceed(time);
+        // The time is normalized, so agents need not be
+        // concerned with the actual internal clock.
 
-				for (Map.Entry<AgentID, Offer> agent : agents.entrySet()) {
-					sum += ((ValueReal) agent.getValue().getBid().getValue(id)).getValue();
-					++count;
-				}
+        // Increase our round timer.
+        ++round;
 
-				proposal.put(id, new ValueReal(lerp(sum / count, ((ValueReal) averages.get(id)).getValue(), willingness)));
-				averages.put(id, new ValueReal(sum / count));
-			} else {
-				throw new UnsupportedOperationException("Unexpected value type!");
-			}
-		}
+        // We need some time to understand our opponents
+        // Means we have enough data and we don't (really) need to worry about nulls
+        if (round <= 2) return new Offer(this.getPartyId(), maxbid);
 
-		// Is the offer good enough?
-		Bid bid = new Bid(this.getUtilitySpace().getDomain(), new HashMap<>(proposal));
-		if(this.getUtilitySpace().getUtility(last) >= this.getUtilitySpace().getUtility(bid)) {
-			System.out.println(getDescription() + ": Accepting offer " + this.getUtilitySpace().getUtility(last) + " " + last);
-			return new Accept(this.getPartyId(), last);
-		} else {
-			// Offer is no good, propose our own
-			System.out.println(getDescription() + ": Proposing offer " + this.getUtilitySpace().getUtility(bid) + " " + bid);
-			return new Offer(this.getPartyId(), bid);
-		}
-	}
+        // Last bid
+        Bid last = history.get(history.size() - 1).getSecond().getBid();
 
-	/**
-	 * This method is called to inform the party that another NegotiationParty chose an Action.
-	 */
-	@Override
-	public void receiveMessage(AgentID sender, Action act) {
-		super.receiveMessage(sender, act);
+        // Find the average
+        Map<Integer, Value> averages = new HashMap<>(maxbid.getValues());
+        Map<Integer, Value> proposal = new HashMap<>(maxbid.getValues());
 
-		System.out.println(getDescription() + ": receiveMessage(" + sender + "," + act + ")");
 
-		if (act instanceof Offer) { // sender is making an offer
-			Offer offer = (Offer) act;
+        averages.forEach((Integer id, Value value) -> {
+            if (value instanceof ValueDiscrete) {
+                // Convert to additive space
+                AdditiveUtilitySpace additiveUtilitySpace = (AdditiveUtilitySpace) utilitySpace;
 
-			// storing last received offer
-			history.add(new Pair<>(sender, offer));
-			if(!agents.containsKey(sender)) agents.put(sender, offer);
-		}
-	}
+                // Get the list of issues
+                List<Issue> issues = additiveUtilitySpace.getDomain().getIssues();
 
-	/**
-	 * A human-readable description for this party.
-	 */
-	@Override
-	public String getDescription() {
-		// Describe the agents mood
-		String[] descriptors = new String[] {"Submissive", "Soft", "Kind", "Reasonable", "Determined", "Firm", "Tough", "Angry", "Mad"};
-		return descriptors[(int) Math.round(clamp(Math.log10(stubbornness) + 2, 0, descriptors.length))] + " " + description;
-	}
+                // Get the issue discrete for the current issue
+                IssueDiscrete issueDiscrete = (IssueDiscrete) issues.get(id - 1);
+                EvaluatorDiscrete evaluatorDiscrete = (EvaluatorDiscrete) additiveUtilitySpace.getEvaluator(id - 1);
 
-	private Bid getMaxUtilityBid() {
-		try {
-			return this.getUtilitySpace().getMaxUtilityBid();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+                // Make a priority queue of the vlaues with their evaluations
+                PriorityQueue<OrderedValue> priorityQueue = new PriorityQueue<>();
+
+                // Check values isn't empty
+                if (!issueDiscrete.getValues().isEmpty()) {
+                    issueDiscrete.getValues().forEach((ValueDiscrete valueDiscrete) -> {
+                        try {
+                            priorityQueue.add(new OrderedValue(valueDiscrete, evaluatorDiscrete.getEvaluation(valueDiscrete)));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    });
+
+                    // Work the way down the priorities
+                    boolean proposalSet = false;
+                    OrderedValue previousOrderedValue;
+                    OrderedValue currentOrderedValue = priorityQueue.poll();
+                    while (!priorityQueue.isEmpty() && !proposalSet) {
+                        previousOrderedValue = currentOrderedValue;
+                        currentOrderedValue = priorityQueue.poll();
+                        if (willingness > currentOrderedValue.evaluation) {
+                            proposalSet = true;
+                            proposal.put(id, previousOrderedValue.value);
+                        }
+                    }
+                    if (!proposalSet) {
+                        proposal.put(id, currentOrderedValue.value);
+                    }
+                }
+
+            } else if (value instanceof ValueInteger) {
+                int sum = 0;
+                int count = 0;
+
+                for (Map.Entry<AgentID, Offer> agent : agents.entrySet()) {
+                    sum += ((ValueInteger) agent.getValue().getBid().getValue(id)).getValue();
+                    ++count;
+                }
+
+                proposal.put(id, new ValueInteger(lerp(sum / count, ((ValueInteger) value).getValue(), willingness)));
+                averages.put(id, new ValueInteger(sum / count));
+            } else if (value instanceof ValueReal) {
+                double sum = 0;
+                int count = 0;
+
+                for (Map.Entry<AgentID, Offer> agent : agents.entrySet()) {
+                    sum += ((ValueReal) agent.getValue().getBid().getValue(id)).getValue();
+                    ++count;
+                }
+
+                proposal.put(id, new ValueReal(lerp(sum / count, ((ValueReal) value).getValue(), willingness)));
+                averages.put(id, new ValueReal(sum / count));
+            } else {
+                throw new UnsupportedOperationException("Unexpected value type!");
+            }
+        });
+
+        // Is the offer good enough?
+        Bid bid = new Bid(this.getUtilitySpace().getDomain(), new HashMap<>(proposal));
+        if (this.getUtilitySpace().getUtility(last) >= this.getUtilitySpace().getUtility(bid)) {
+            System.out.println(getDescription() + ": Accepting offer " + this.getUtilitySpace().getUtility(last) + " " + last);
+            return new Accept(this.getPartyId(), last);
+        } else {
+            // Offer is no good, propose our own
+            System.out.println(getDescription() + ": Proposing offer " + this.getUtilitySpace().getUtility(bid) + " " + bid);
+            return new Offer(this.getPartyId(), bid);
+        }
+    }
+
+    protected class OrderedValue implements Comparable<OrderedValue> {
+        private Value value;
+        private Double evaluation;
+
+        public OrderedValue(Value value, double evaluation) {
+            this.value = value;
+            this.evaluation = evaluation;
+        }
+
+        public Value getValue() {
+            return value;
+        }
+
+        public void setValue(Value value) {
+            this.value = value;
+        }
+
+        public double getEvaluation() {
+            return evaluation;
+        }
+
+        public void setEvaluation(double evaluation) {
+            this.evaluation = evaluation;
+        }
+
+        @Override
+        public int compareTo(OrderedValue o) {
+            return this.evaluation.compareTo(o.evaluation);
+        }
+    }
+
+
+    /**
+     * This method is called to inform the party that another NegotiationParty chose an Action.
+     */
+    @Override
+    public void receiveMessage(AgentID sender, Action act) {
+        super.receiveMessage(sender, act);
+
+        System.out.println(getDescription() + ": receiveMessage(" + sender + "," + act + ")");
+
+        if (act instanceof Offer) { // sender is making an offer
+            Offer offer = (Offer) act;
+
+            // storing last received offer
+            history.add(new Pair<>(sender, offer));
+            if (!agents.containsKey(sender)) agents.put(sender, offer);
+        }
+    }
+
+    /**
+     * A human-readable description for this party.
+     */
+    @Override
+    public String getDescription() {
+        // Describe the agents mood
+        String[] descriptors = new String[]{"Submissive", "Soft", "Kind", "Reasonable", "Determined", "Firm", "Tough", "Angry", "Mad"};
+        return descriptors[(int) Math.round(clamp(Math.log10(stubbornness) + 2, 0, descriptors.length))] + " " + description;
+    }
+
+    private Bid getMaxUtilityBid() {
+        try {
+            return this.getUtilitySpace().getMaxUtilityBid();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
